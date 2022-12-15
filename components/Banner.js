46a6e0d20886a4ec775eb3c62f90df4128e3edab
Cecilia Fernandez Aguilera <ceci.aguilera@zohomail.com>
2022-12-14 22:02:40 -0600

    Changed principal Banner under Navbar

--- components/Banner.js
+++ components/Banner.js
@@ -5,13 +5,22 @@ import { Container } from "react-bootstrap"
 const Banner = ({ image_source, fromPublic = true, tagLineBool = false, tagLine = '' }) => {
 
     return (
-        <div className={styles.banner_div}>
-            {tagLineBool ? <div className={styles.banner_tagline_div}>{tagLine}</div> : <></>}
-            <div className={styles.banner_img_cover_div}> </div>
-            {fromPublic ? <div className={styles.banner_img_div}>
-                <Image src={image_source} alt='Habaneras de Lino Banner' layout="fill" />
-            </div>
-                : <div className={styles.banner_img_div_wrapper}><img src={image_source} alt='Habaneras de Lino Banner' className={styles.banner_img} /> </div>
+        <div>
+            {fromPublic ?
+                <div className={styles.banner_div_p}>
+                    {tagLineBool ? <div className={styles.banner_tagline_div_p}>{tagLine}</div> : <></>}
+
+                    <div className={styles.banner_img_cover_div_p}> </div>
+
+                    <div className={styles.banner_img_div_p}>
+                        <Image src={image_source} alt='Habaneras de Lino Banner' layout="fill" />
+                    </div>
+                </div>
+                : <div className={styles.banner_div}>
+                    {tagLineBool ? <div className={styles.banner_tagline_div}>{tagLine}</div> : <></>}
+                    <div className={styles.banner_img_cover_div}> </div>
+                    : <div className={styles.banner_img_div_wrapper}><img src={image_source} alt='Habaneras de Lino Banner' className={styles.banner_img} /> </div>
+                </div>
             }
         </div>
     )
